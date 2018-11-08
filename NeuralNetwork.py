ac88881dd2e46fffbf5951450127d316084416f2
Daniel Hornberger <hor12020@byui.edu>
2018-11-07 21:29:13 -0700

    Implemented weight updates and acheived training

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -12,14 +12,19 @@ import pandas as pd
 from sklearn import datasets
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
+
+accuracy_graph = []
 
 class NNetNode:
     def __init__(self):
         self.inputs = []
         self.weights = []
+        self.old_weights = []
         self.output = -1
         self.thresh = 0
+        self.error = 0
 
     def initialize_weights(self, num_weights):
         self.weights = randn(num_weights+1)
@@ -31,12 +36,7 @@ class NNetNode:
     def _activation_fun(self, raw_output):
         # Sigmoid function
         sigmoid_val = 1/(1+np.e**(-1*raw_output))
-
-        # Return 1 if closer to 1
-        if sigmoid_val >= 0.5:
-            return 1
-        else:
-            return 0
+        return sigmoid_val
     
     def get_output(self):
         # Calculate the summation of inputs * weights
@@ -52,6 +52,7 @@ class NNetClassifier:
         self.net = [] # A list of lists of nodes. Each list of nodes represents a layer
         self.hidden_layers = [] # A list of lists of nodes. Each list of nodes represents a layer.
         self.outputs = []
+        self.learning_rate = 0.1
     
     # Take a list of layer sizes. Each size in the list will create a layer consisting of 'size' nodes
     # The hidden layers must be specified as well as the size of the output layer
@@ -86,24 +87,78 @@ class NNetClassifier:
         for node in self.net[layer_num - 1]:
             prev_layer_outputs.append(node.get_output())
             
-        
         # Use the outputs of the previous layer as the inputs to this layer
         for node in self.net[layer_num]:
             node.set_inputs(prev_layer_outputs)
         
         # Recursively propogate to the next layer
         self._propogate_through_hidden_layers(layer_num + 1)
+        
+    def _set_output_error(self, node, target):
+        activation = node.output
+        node.error = activation*(1-activation)*(activation-target)
+        error_graph.append(node.error)
+    
+    # node: the node for which I'm seeking the error
+    # node_index: the index of the node in its layer to be used for getting the right weight
+    # layer_index: the index of the layer in which this node resides
+    def _set_hidden_error(self, node, node_index, layer_index):
+        activation = node.output
+        error = 0
+        
+        k_layer = self.net[layer_index+1]
+        
+        for k_node in k_layer:
+            error += activation*(1-activation)*k_node.old_weights[node_index]*k_node.error
+        
+        node.error = error
+        
+    def _update_weights(self, layer_index):
+        for node in self.net[layer_index]:
+            # Preserve the weights for subsequent weight updates to refer to the correct weight value
+            node.old_weights = node.weights 
+            
+            # Update the weights
+            for i in range(len(node.weights)):
+                node.weights[i] = node.old_weights[i]-(self.learning_rate*node.error*node.output)
+                
+        
+    def _back_propogate(self, targets):
+        
+        # Obtain the outputs of the final layer (This was usually done in predict before back prop)
+        self._get_final_outputs()
+        
+        # Calculate the errors of the output layer nodes
+        target_index = 0
+        for node in self.net[-1]:
+            self._set_output_error(node, targets[target_index])
+            target_index += 1
+            
+        # Update the weights to the output nodes
+        self._update_weights(-1)
+        
+        # Loop through each of the hidden layers going backwards through the net, skip output layer
+        for layer_index in range(len(self.net)-2, -1, -1):
+            # Calculatet the errors of the nodes in the hidden layers
+            for node_index in range(len(self.net[layer_index])):
+                self._set_hidden_error(self.net[layer_index][node_index], node_index, layer_index)
+            
+            # Update the weights of the nodes in the hidden layer now that we have the errors
+            self._update_weights(layer_index)
+            
             
     # Train the NNet
-    def fit(self, data_train, data_targets):
+    def fit(self, data_train, data_targets, is_first):
         
         first_layer_num = 0
         
         # Initialize the weights of the first layer since we know how many inputs now
-        for node in self.net[0]:
-            node.initialize_weights(len(data_train[0]))
+        if is_first:
+            for node in self.net[0]:
+                node.initialize_weights(len(data_train[0]))
         
         # Send each row through the NNet
+        target_index = 0
         for row in data_train:            
             # Set the inputs of each node in the first layer
             for node in self.net[first_layer_num]:
@@ -111,14 +166,30 @@ class NNetClassifier:
               
             # Propogate the inputs and outputs through each layer
             self._propogate_through_hidden_layers(first_layer_num + 1)
-#                _back_propogate() ??
+            
+            # Get the target values for this row as a list from the pandas dataframe
+            targets_list = data_targets.iloc[target_index].tolist()
+            
+            self._back_propogate(targets_list)
+            
+            target_index += 1
                 
     def _get_final_outputs(self):
         outputs = []
         
         # Get the outputs from the last layer of nodes in the net
         for node in self.net[-1]:
-            outputs.append(node.get_output())
+       
+            sigmoid_val = node.get_output()
+            prediction = -1
+            
+            # Return 1 if closer to 1
+            if sigmoid_val >= 0.5:
+                prediction = 1
+            else:
+                prediction = 0
+                
+            outputs.append(prediction)
         return outputs
     
     # Run the list of attributes through the network and return the outputs        
@@ -138,38 +209,25 @@ class NNetClassifier:
             predictions.append(self._get_final_outputs())    
         return predictions
 
-def one_hot_encode_targets(targets):
+#-----------------------------------------------------------------------------
+
+def one_hot_encode_iris_targets(targets):
     one_hot_cols = ["0", "1", "2"]
     one_hot_targets = pd.get_dummies(targets, columns=one_hot_cols)
     return one_hot_targets
 
-def main(): 
-    # Load the iris dataset
-    print("Loading the data...")
-    dataset = datasets.load_iris()
+def one_hot_encode_b_cancer_targets(targets):
+    one_hot_cols = ["0", "1"]
+    one_hot_targets = pd.get_dummies(targets, columns=one_hot_cols)
+    return one_hot_targets
 
-    # Obtain a normalizing scaler for scaling new data if added later
-    std_scaler = preprocessing.StandardScaler().fit(dataset.data)
-    
-    # Normalize the data
-    std_data = preprocessing.scale(dataset.data)
-    
-    # Randomize the dataset and divide the data for testing and training 
-    # The following line uses the iris dataset from sklearn
-    data_train, data_test, targets_train, targets_test = train_test_split(
-            std_data, dataset.target, test_size = 0.30, random_state=42)
-    
-    one_hot_targets_train = one_hot_encode_targets(targets_train)
-    one_hot_targets_test  = one_hot_encode_targets(targets_test)
-    
-    # Obtain a classifier. 
-    classifier = NNetClassifier()
-    # Two hidden layers of 12 and 4 nodes, output layer of 3 nodes
-    classifier.configure_hidden_layers([12, 4, 3]) 
+def do_epoch(classifier, data_train, data_test, one_hot_targets_train, one_hot_targets_test, epoch_cnt, is_first_iter):
+    print("-------------------------------------------------")
+    print("Epoch {}".format(epoch_cnt))
     
     # Fit the data
     print("Training...")
-    classifier.fit(data_train, one_hot_targets_train)
+    classifier.fit(data_train, one_hot_targets_train, is_first_iter)
     
     # Get the predicted targets
     print("Testing...")
@@ -188,7 +246,58 @@ def main():
             correct_count+=1
             
     accuracy = float(correct_count) / float(num_predictions)
+    accuracy_graph.append(accuracy)
     print("Total Accuracy: {:.2f}%".format(accuracy * 100.0))
+
+def main(): 
+    # Load the iris dataset
+    print("Loading the data...")
+    dataset = datasets.load_iris()
+#    dataset = datasets.load_breast_cancer()
+
+    # Obtain a normalizing scaler for scaling new data if added later
+    std_scaler = preprocessing.StandardScaler().fit(dataset.data)
     
+    # Normalize the data
+    std_data = preprocessing.scale(dataset.data)
+    
+    # Randomize the dataset and divide the data for testing and training 
+    # The following line uses the iris dataset from sklearn
+    data_train, data_test, targets_train, targets_test = train_test_split(
+            std_data, dataset.target, test_size = 0.30, random_state=42)
+    
+    # Iris Dataset
+    one_hot_targets_train = one_hot_encode_iris_targets(targets_train)
+    one_hot_targets_test  = one_hot_encode_iris_targets(targets_test)
+    
+    # Breast Cancer Dataset
+#    one_hot_targets_train = one_hot_encode_b_cancer_targets(targets_train)
+#    one_hot_targets_test  = one_hot_encode_b_cancer_targets(targets_test)
+    
+    # Obtain a classifier. 
+    classifier = NNetClassifier()
+    
+    # Two hidden layers of 20 nodes, output layer of 3 nodes
+    classifier.configure_hidden_layers([20, 20, 3]) # Iris Dataset
+#    classifier.configure_hidden_layers([40, 40, 2])  # Breast Cancer Dataset 
+    
+
+    # The first training instance - This randomizes the weights at first
+    do_epoch(classifier, data_train, data_test, one_hot_targets_train, one_hot_targets_test, 1, True)
+
+    # Number of epochs to train the net
+    for i in range(0, 99):
+        epoch_cnt = i + 2 # The first epoch has already been done
+        do_epoch(classifier, 
+                 data_train, 
+                 data_test, 
+                 one_hot_targets_train, 
+                 one_hot_targets_test, 
+                 epoch_cnt, 
+                 False)
+
+    # Graph the accuracy        
+    plt.plot(accuracy_graph)
+    plt.show()    
 
 main()
